37084444ffcc4e01578056790edf507c1a7697f4
Akos Stegner <a.stegner@mito.hu>
2022-10-03 11:02:50 +0200

    Create manage users

--- components/site-header.client.vue
+++ components/site-header.client.vue
@@ -13,6 +13,7 @@ const onSignOut = () => {
       </div>
       <div class="flex items-center space-x-4">
         <NuxtLink to="/manage-video" class="px-2 py-1"> Manage Videos </NuxtLink>
+        <NuxtLink to="/manage-users" class="px-2 py-1"> Manage Users </NuxtLink>
         <NuxtLink v-if="!store.isLoggedIn" to="/sign-in"> Sign In </NuxtLink>
         <NuxtLink v-if="!store.isLoggedIn" to="/sign-up"> Sign Up </NuxtLink>
         <a v-if="store.isLoggedIn" href="#" @click.prevent="onSignOut">Sign Out</a>
---
+++ server/api/users.js
@@ -0,0 +1,12 @@
+import { serverSupabaseClient } from "#supabase/server";
+
+export default defineEventHandler(async (event) => {
+  const { from, to } = useQuery(event);
+  const supabase = serverSupabaseClient(event);
+
+  const { data, count } = await supabase
+    .from("users")
+    .select("*", { count: "exact" })
+    .range(from, to);
+  return { videos: data, total: count };
+});
